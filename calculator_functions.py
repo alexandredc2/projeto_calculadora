46d29b5f631cb2d539116c54e831b3c2973536a4
Alexandre Caspechaque <ale.dc240@gmail.com>
2024-05-01 11:49:35 -0300

    Created first calculator functions, and a previous terminal simulator for the application

---
+++ calculator_functions.py
@@ -0,0 +1,11 @@
+def sum(num1, num2):
+    return float(num1)+float(num2)
+
+def sub(num1, num2):
+    return float(num1)-float(num2)
+
+def div(num1, num2):
+    return float(num1)/float(num2)
+
+def mul(num1, num2):
+    return float(num1)*float(num2)
--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-print("This calculator is designed by Alexandre D. Caspechaque")
-print("Date 01/05/2024")
+import calculator_functions as calc
+import os
+
+print("##########################################################")
+print("This is a Calculator developed by Alexandre D. Caspechaque")
+print("##########################################################\n")
+
+active = True
+firstRun = True
+
+while active:
+    if not firstRun:
+        num1 = result
+        print(num1)
+        operator = input()
+        num2 = input()
+
+        if num2 == "exit" or num2 == "exit":
+            active = False
+            break
+
+    if firstRun:
+        num1 = input("num1: ")
+        operator = input("operator: ")
+        num2 = input("num2: ")
+        firstRun = False
+
+    if operator == "+":
+        result = calc.sum(num1, num2)
+        os.system("cls")
+    elif operator == "-":
+        result = calc.sub(num1, num2)
+        os.system("cls")
+    elif operator == "/":
+        result = calc.div(num1, num2)
+        os.system("cls")
+    elif operator == "*":
+        result = calc.mul(num1, num2)
+        os.system("cls")
